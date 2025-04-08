6ec01e84701d2a2d6b3ab78667efaa044ca3c6f5
Miguwatt <miguelpublic222@gmail.com>
2025-04-08 12:19:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  const content = document.getElementById("main-content");
 
-
-const apiKey = 'f18d7d252ec5c71a259b443fcf020c46';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
-
-const locationInput = document.getElementById('locationInput');
-const searchButton = document.getElementById('searchButton');
-const locationElement = document.getElementById('location');
-const temperatureElement = document.getElementById('temperature');
-const descriptionElement = document.getElementById('description');
-
-searchButton.addEventListener('click', () => {
-    const location = locationInput.value;
-    if (location) {
-        fetchWeather(location);
-    }
+  loader.style.display = "none";
+  content.style.display = "block";
 });
-
-function fetchWeather(location) {
-    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            locationElement.textContent = data.name;
-            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
-            descriptionElement.textContent = data.weather[0].description;
-        })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-        });
-}
-
