156b24da46d985f90f46024ba6b2e0d4189c3202
Miguwatt <miguelpublic222@gmail.com>
2025-04-10 14:51:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  const content = document.getElementById("main-content");
 
-  loader.style.display = "none";
-  content.style.display = "block";
-});
